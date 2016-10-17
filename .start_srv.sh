39f6be31bf0471f2ac9f5ab83a7119b2a7b88644
Erik Nelson <erik@nsk.io>
2016-10-17 12:15:42 -0400

    Add localhost go server target

--- .start_srv.sh
+++ .start_srv.sh
@@ -6,6 +6,9 @@ fusor_config_dir=$project_root/config/fusor
 if ! [ -z "$MOCKS_ENABLED" ]; then
   echo "Building to target mocked environment..."
   ln -sf $fusor_config_dir/mocked.js $fusor_config_dir/config.js
+elif ! [ -z "$LOCAL" ]; then
+  echo "Building to target local environment..."
+  ln -sf $fusor_config_dir/local.js $fusor_config_dir/config.js
 else
   echo "Building to target vagrant environment..."
   ln -sf $fusor_config_dir/vagrant.js $fusor_config_dir/config.js
