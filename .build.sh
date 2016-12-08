bbb6536e88593156d43d1359f11bbc2105a52b34
jesus m. rodriguez <jmrodri@gmail.com>
2016-12-08 13:11:41 -0500

    change fusor to cap

--- .build.sh
+++ .build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 project_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-fusor_config_dir=$project_root/config/fusor
+fusor_config_dir=$project_root/config/cap
 
 ln -sf $fusor_config_dir/vagrant.js $fusor_config_dir/config.js
 npm run __build
