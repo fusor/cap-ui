233bf89f63d428a60223be887da4ef17c799db0d
Erik Nelson <erik@nsk.io>
2016-12-09 10:56:31 -0500

    Sprint4 updates
    * workengine based push updates
    * catalog search support

--- src/Main.jsx
+++ src/Main.jsx
@@ -8,12 +8,45 @@ import { Provider } from 'react-redux';
 import Layout from './base/components/Layout';
 import rootReducer from './rootReducer';
 import Routes from './Routes';
+import actions, { actionTypes } from './content/actions';
+import config from '../config/cap/config';
 
 import io from 'socket.io-client';
 
+
+////////////////////////////////////////////////////////////
+// job socket middleware
+////////////////////////////////////////////////////////////
+const socket = io(`${config.capServer}/socket.io`);
+
+socket.on('connect', () => dd('connected'))
+socket.on('disconnect', () => dd('disconnected'))
+
+const jobsMiddleware = store => next => action => {
+  if(action.type != actionTypes.SUBSCRIBE_JOB) {
+    next(action);
+    return;
+  }
+
+  const { token } = action.payload;
+  const { nuleculeId } = action.meta;
+
+  // TODO: use true multichannels instead of mux/demuxing the same channel
+  socket.on('firehose', msg => {
+    const m = JSON.parse(msg);
+    store.dispatch(actions.updateJob(m.job_token, m.msg))
+  });
+};
+////////////////////////////////////////////////////////////
+
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunkMiddleware, promiseMiddleware(), loggerMiddleware())
+  applyMiddleware(
+    thunkMiddleware,
+    promiseMiddleware(),
+    jobsMiddleware,
+    loggerMiddleware()
+  )
 );
 
 const mountPoint = document.getElementById('main');
@@ -24,22 +57,6 @@ ReactDOM.render(
   </Provider>,
   mountPoint
 );
-
-dd('attempting to connect to socket');
-
-const socket = io();
-
-socket.on('connect', () => {
-  dd('connected')
-});
-
-socket.on('firehose', (msg) => {
-  dd('ff -> ' + msg)
-});
-
-socket.on('disconnect', () => {
-  dd('disconnected')
-});
 
 function dd(msg) {
   console.debug(msg);
--- src/content/components/NuleculeReviewPage.jsx
+++ src/content/components/NuleculeReviewPage.jsx
@@ -3,75 +3,31 @@ import { connect } from 'react-redux';
 import { Jumbotron } from 'react-bootstrap';
 import actions from '../actions';
 import axios from 'axios';
-import { getHealthCheckUrl } from  '../../shared/api';
 
 import Layout from '../../base/components/Layout';
 
 class NuleculeReviewPage extends React.Component {
-  componentWillReceiveProps(nextProps) {
-    const { registry, nuleculeId, deploymentId } = this.props.params;
-    const prevStatus = this.props.deployments[deploymentId].status;
-    const nextStatus = nextProps.deployments[deploymentId].status;
-
-    if(prevStatus === nextStatus) {
-      return;
-    }
-
-    if(nextStatus === "Waiting for pods...") {
-      const nulecule = this.props.nulecules[fullNuleculeId(registry, nuleculeId)];
-      const endpoint = this.getEndpoint(nulecule);
-
-      // TODO: Check if they provided the protocol...
-      const healthCheck = new HealthCheck(
-        endpoint,
-        this.handleSuccessfulHealthCheck.bind(this),
-        this.handleFailedHealthCheck.bind(this));
-
-      healthCheck.run();
-
-      this.setState({
-        endpoint,
-        healthCheck,
-        status: HealthCheck.StatusEnum.RUNNING
-      });
-    }
-  }
-  handleSuccessfulHealthCheck() {
-    const { deploymentId } = this.props.params;
-    this.props.deployStatusUpdate(deploymentId, "Success!");
-
-    this.setState({
-      ...this.state,
-      healthCheck: null,
-      status: HealthCheck.StatusEnum.SUCCESS
-    });
-  }
-  handleFailedHealthCheck() {
-    const { deploymentId } = this.props.params;
-    this.props.deployStatusUpdate(deploymentId, "Timed out");
-
-    this.setState({
-      ...this.state,
-      healthCheck: null,
-      status: HealthCheck.StatusEnum.FAILURE
-    });
-  }
   render() {
     const { registry, nuleculeId, deploymentId } = this.props.params;
+    const fullId = fullNuleculeId(registry, nuleculeId);
 
     let nulecule;
-    if(this.props.nulecules && this.props.nulecules[nuleculeId]) {
-      nulecule = this.props.nulecules[nuleculeId];
+    if(this.props.nulecules && this.props.nulecules[fullId]) {
+      nulecule = this.props.nulecules[fullId];
     }
 
     let deployment;
-    let deploymentStatus = "Pending...";
+    let displayStatus = "Pending...";
     if(this.props.deployments && this.props.deployments[deploymentId]) {
       deployment = this.props.deployments[deploymentId];
-      deploymentStatus = deployment.status;
+      if(deployment.job && this.props.jobs && this.props.jobs[deployment.job]) {
+        const job = this.props.jobs[deployment.job];
+        displayStatus = job.msg; // Disply last received message
+      }
     }
 
     const onDeployClick = this.props.deploy.bind(this, registry, nuleculeId, deploymentId);
+    const displayEndpoint = nulecule ? 'http://' + this.getEndpoint(nulecule) : "no nulecule.";
 
     return (
       <Layout>
@@ -80,22 +36,20 @@ class NuleculeReviewPage extends React.Component {
         </Jumbotron>
         <Jumbotron>
           <h3>ID: {deploymentId}</h3>
-          <h3>Status: {deploymentStatus}</h3>
-          {this.endpointInfo()}
+          <h3>Status: {displayStatus}</h3>
+          {(() => {
+            if(displayStatus === 'Full deployment finished.') {
+              return <h3>Endpoint <a href={displayEndpoint}>{displayEndpoint}</a></h3>
+            }
+          })()}
         </Jumbotron>
         {(() => {
-          return deploymentStatus === "Pending..." ?
+          return displayStatus === "Pending..." ?
             <button className="btn btn-primary" onClick={onDeployClick}>Deploy</button> :
             null
         })()}
       </Layout>
     );
-  }
-  endpointInfo(deploymentInfo) {
-    if(this.state && this.state.status === HealthCheck.StatusEnum.SUCCESS) {
-      const endpoint = `http://${this.state.endpoint}`;
-      return (<h3><a href={endpoint}>{endpoint}</a></h3>)
-    }
   }
   getEndpoint(nulecule) {
     const nodes = Object.keys(nulecule);
@@ -110,81 +64,11 @@ function fullNuleculeId(registry, nuleculeId) {
   return `${registry}/${nuleculeId}`;
 }
 
-class HealthCheck {
-  constructor(endpoint, successCb, failureCb) {
-    this.endpoint = endpoint;
-    this.status = HealthCheck.StatusEnum.RUNNING;
-
-    this._checkUrl = getHealthCheckUrl();
-    this._successCb = successCb;
-    this._failureCb = failureCb;
-    this._TICK_PERIOD = 10000;
-    this._TIMEOUT = 180000; // 3 min timeout
-    this._debugCount = 0;
-  }
-  run() {
-    this._startTime = Date.now();
-    this._interval = setInterval(this._tick.bind(this), this._TICK_PERIOD);
-  }
-  _tick() {
-    this._lastTick = Date.now();
-
-    // Check if we've timed out, if not, ping the service to see if the service
-    // is up yet. If it is, report things as healthy and stop.
-    if(this._isTimeUp()) {
-      this._stopWithStatus(HealthCheck.StatusEnum.FAILURE);
-    } else {
-      this._pingService().then((status) => {
-        if(status === HealthCheck.StatusEnum.SUCCESS &&
-           this.status !== HealthCheck.StatusEnum.FAILURE)
-        {
-          this._stopWithStatus(HealthCheck.StatusEnum.SUCCESS);
-        }
-      });
-    }
-  }
-  _isTimeUp() {
-    const elapsed = Date.now() - this._startTime;
-    return elapsed >= this._TIMEOUT;
-  }
-  _stopWithStatus(status) {
-    clearInterval(this._interval);
-    this.status = status;
-
-    if(status === HealthCheck.StatusEnum.SUCCESS) {
-      this._successCb();
-    } else {
-      this._failureCb();
-    }
-  }
-  _pingService() {
-    const endpoint = this.endpoint;
-    return new Promise((resolve, _) => {
-      axios.post(this._checkUrl, {host: endpoint}).then(response => {
-        if(response.data.is_alive) {
-          resolve(HealthCheck.StatusEnum.SUCCESS);
-        } else {
-          resolve(HealthCheck.StatusEnum.RUNNING);
-        }
-      });
-    });
-  }
-  get runtime() {
-    return this._lastTick - this._startTime;
-  }
-  static get StatusEnum() {
-    return {
-      RUNNING: 1,
-      SUCCESS: 2,
-      FAILURE: 3
-    };
-  }
-}
-
 const mapStateToProps = (state) => {
   return {
     nulecules: state.nulecules,
-    deployments: state.deployments
+    deployments: state.deployments,
+    jobs: state.jobs,
   };
 };
 
