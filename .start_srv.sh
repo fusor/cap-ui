f9c8e05f98132d8001b7be9b6ab30ca785ae3f4e
Erik Nelson <erik@nsk.io>
2016-12-09 10:51:41 -0500

    Param runtime exe for srv

--- .start_srv.sh
+++ .start_srv.sh
@@ -3,9 +3,12 @@
 project_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cap_config_dir=$project_root/config/cap
 
+exe=nodemon
+
 if ! [ -z "$MOCKS_ENABLED" ]; then
   echo "Building to target mocked environment..."
   ln -sf $cap_config_dir/mocked.js $cap_config_dir/config.js
+  exe=node
 elif ! [ -z "$LOCAL" ]; then
   echo "Building to target local environment..."
   ln -sf $cap_config_dir/local.js $cap_config_dir/config.js
@@ -14,5 +17,4 @@ else
   ln -sf $cap_config_dir/vagrant.js $cap_config_dir/config.js
 fi
 
-#node $project_root/dev_srv.js
-nodemon $project_root/dev_srv.js
+$exe $project_root/dev_srv.js
